c25bcf37114d4edbe1872e58898e47a7e9ff74cc
Harsh Rohila <rohilaharsh@gmail.com>
2020-07-18 22:45:10 +0530

    added scales

--- src/Components/Numbers/index.js
+++ src/Components/Numbers/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Styled from 'styled-components'
 
+const SCALE_CHAR = 'l';
+
 const Numbers = Styled.div`
   position: absolute;
   top: 50%;
@@ -8,26 +10,38 @@ const Numbers = Styled.div`
 `
 const Number = Styled.span`
   position: absolute;
-  transform: translate(-50%, -50%);
+  transform-origin: top center;
+  transform: ${props => `rotate(${props.rotation}deg) translate(-50%, -50%)`};
 `
 export default () => {
-  let numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2];
+  let numbers = ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '1', '2'];
+
+  numbers = numbers.map(num => [num, SCALE_CHAR, SCALE_CHAR, SCALE_CHAR, SCALE_CHAR])
+            .reduce((acc, curr) => {
+              return acc.concat(curr);
+            }, []);
  
   let numberObjects = [];
   numbers.forEach((num, i) => {
-    let h = 120;
-    let a = 360 / numbers.length; 
+    let r = 140;
+    let a = 360 / numbers.length;
+    const angle = toRadians(a * i);
     let obj = {
         value: num,
-        top: h * Math.sin(toRadians(a * i)),
-        left: h * Math.cos(toRadians(a * i))
+        top: r * Math.sin(angle),
+        left: r * Math.cos(angle),
+        rotation: num === SCALE_CHAR ? ((a * i) - 90) : 0,
+        key: i
     };
     numberObjects.push(obj);
   });
   return (
     <Numbers>
-      {numberObjects.map(({ value, top, left}) => 
-        <Number key={value} style={{top: `${top}px`, left: `${left}px`}}>
+      {numberObjects.map(({ value, top, left, rotation, key}) => 
+        <Number 
+          key={key} 
+          rotation={rotation}
+          style={{top: `${top}px`, left: `${left}px`}}>
           {value}
         </Number>)
       }
